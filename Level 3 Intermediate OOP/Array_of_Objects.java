0e088f4436e1885d8d949d3f9f37555f230c9d0c
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-04 13:07:07 +0530

    m

---
+++ Level 3 Intermediate OOP/Array_of_Objects.java
@@ -0,0 +1,5 @@
+public class Array_of_Objects {
+    public static void main(String[] args) {
+        // Array of Objects
+    }
+}
---
+++ Level 3 Intermediate OOP/Object_as_Method_Parameter.java
@@ -0,0 +1,5 @@
+public class Object_as_Method_Parameter {
+    public static void main(String[] args) {
+        // Object as Method Parameter
+    }
+}
