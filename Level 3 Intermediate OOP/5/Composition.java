b2342361760accdc33c50d9153ea2466361a380a
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-06 01:15:55 +0530

    OOP

--- Level 3 Intermediate OOP/5/Composition.java
+++ Level 3 Intermediate OOP/5/Composition.java
@@ -1,5 +1,64 @@
 public class Composition {
     public static void main(String[] args) {
         // Composition
+
+        /*
+         * Definition
+         * Composition is a relationship where one class contains another class as a
+         * part of it, and the contained object cannot exist independently of the parent
+         * object.
+         * In simple words:
+         * One object owns another object.
+         * 
+         * Example:
+         * Car HAS-A Engine
+         * House HAS-A Room
+         * Human HAS-A Heart
+         * If the parent object is destroyed, the contained object also gets destroyed.
+         * 
+         * ✅ Concept
+         * In Composition:
+         * One class creates and controls the lifetime of another object.
+         * The contained object does not exist separately.
+         * It represents a strong relationship.
+         * Example idea:
+         * Car
+         * └── Engine
+         * The engine belongs only to that car.
+         */
+
+        Laptop l1 = new Laptop("Dell", 5000);
+        Laptop l2 = new Laptop("HP", 4500);
+
+        l1.displayLaptop();
+        l2.displayLaptop();
+
+    }
+}
+
+class Battery {
+    int capacity;
+
+    Battery(int capacity) {
+        this.capacity = capacity;
+    }
+
+    void showBattery() {
+        System.out.println("Battery Capacity : " + capacity);
+    }
+}
+
+class Laptop {
+    String brand;
+    Battery battery;
+
+    Laptop(String brand, int capacity) {
+        this.brand = brand;
+        battery = new Battery(capacity); // Composition
+    }
+
+    void displayLaptop() {
+        System.out.println("Laptop Brand : " + brand);
+        battery.showBattery();
     }
 }
--- Level 3 Intermediate OOP/6/Aggregation.java
+++ Level 3 Intermediate OOP/6/Aggregation.java
@@ -1,5 +1,70 @@
 public class Aggregation {
     public static void main(String[] args) {
         // Aggregation
+
+        /*
+         * Definition
+         * Aggregation is a HAS-A relationship where one class contains another class,
+         * but the contained object can exist independently.The child object can exist
+         * without the parent object.
+         * Example
+         * Student HAS-A Address
+         * Department HAS-A Teacher
+         * Library HAS-A Books
+         * If the Department is deleted, the Teacher can still exist.
+         * 
+         * Concept (Simple Understanding)
+         * In Aggregation:
+         * One class uses another class object
+         * The object is created outside the class
+         * The parent class does not control its lifetime
+         * 
+         * | Feature | Composition | Aggregation |
+         * | ------------------------- | ------------------- | -------------------- |
+         * | Object creation | Inside parent class | Outside parent class |
+         * | Relationship strength | Strong | Weak |
+         * | Child object independent? | No | Yes |
+         * 
+         * Department
+         * |
+         * ---> Teacher
+         * Teacher can exist even if department object is deleted.
+         * 
+         */
+
+        Book book1 = new Book("Java Bascis", "James");
+        Library library1 = new Library("City Library", book1);
+
+        library1.showLibrary();
+        book1.showBook();
+    }
+}
+
+class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    void showBook() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+class Library {
+    String libraryName;
+    Book book1;
+
+    Library(String libraryName, Book book1) {
+        this.libraryName = libraryName;
+        this.book1 = book1;
+    }
+
+    void showLibrary() {
+        System.out.println("Library Name: " + libraryName);
     }
 }
