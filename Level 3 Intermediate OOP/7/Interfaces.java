ac609d0696b12e7a38ee64060bc6aee8f6ea15ae
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-06 15:37:13 +0530

    Object Oriented Programming In Java

--- Level 3 Intermediate OOP/7/Interfaces.java
+++ Level 3 Intermediate OOP/7/Interfaces.java
@@ -1,5 +1,74 @@
 public class Interfaces {
     public static void main(String[] args) {
         // Interfaces
+
+        /*
+         * Definition:
+         * An Interface in Java is a blueprint of a class that contains abstract methods
+         * and constants.
+         * It is used to achieve:
+         * Abstraction
+         * Multiple inheritance
+         * A class implements an interface and must provide the implementation of its
+         * methods.
+         * 
+         * Concept:
+         * Think of an interface like a contract / rule book.
+         * Example:
+         * Interface says:
+         * Vehicle must have:
+         * start()
+         * stop()
+         * Any class that implements it must follow the rule.
+         * Example classes:
+         * Car
+         * Bike
+         * Truck
+         * All must define:
+         * start()
+         * stop()
+         * 
+         * Syntax:
+         * Interface
+         * interface InterfaceName {
+         * void method1();
+         * void method2();
+         * }
+         * Class implementing interface
+         * class ClassName implements InterfaceName {
+         * public void method1() {
+         * // implementation
+         * }
+         * public void method2() {
+         * // implementation
+         * }
+         * }
+         * 
+         * Important Rules:
+         * Interface methods are public and abstract by default
+         * Variables are public static final (constants)
+         * Objects cannot be created for an interface
+         * A class must implement all methods
+         */
+
+        Fan fan = new Fan();
+        fan.turnOff();
+        fan.turnOn();
+    }
+}
+
+interface Device {
+    void turnOn();
+
+    void turnOff();
+}
+
+class Fan implements Device {
+    public void turnOn() {
+        System.out.println("Fan is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Fan is OFF");
     }
 }
--- Level 3 Intermediate OOP/8/Abstract_Classes.java
+++ Level 3 Intermediate OOP/8/Abstract_Classes.java
@@ -1,5 +1,90 @@
 public class Abstract_Classes {
     public static void main(String[] args) {
         // Abstract Classes
+
+        /*
+         * Definition:
+         * An abstract class is a class that cannot be instantiated (cannot create
+         * objects) and is meant to be extended by other classes.
+         * It can contain:
+         * Abstract methods (methods without body)
+         * Normal methods (methods with body)
+         * Variables
+         * Constructors
+         * 
+         * Concept:
+         * Think of an abstract class as a blueprint.
+         * Example:
+         * Animal is a general concept.
+         * Animals can eat and sleep, but the sound they make differs.
+         * So we create:
+         * Animal
+         * |
+         * |--- Dog
+         * |--- Cat
+         * Animal defines common behavior, but specific classes implement details.
+         * 
+         * Important Rules:
+         * Abstract class is declared using:
+         * abstract class ClassName
+         * Abstract methods must be declared using:
+         * abstract void methodName();
+         * A class that extends an abstract class must implement all abstract
+         * methods.
+         * You cannot create object of abstract class
+         * Not allowed
+         * Animal a = new Animal();
+         * 
+         * Syntax:
+         * abstract class Animal {
+         * abstract void makeSound(); // abstract method
+         * void sleep() { // normal method
+         * System.out.println("Animal sleeping");
+         * }
+         * }
+         * class Dog extends Animal {
+         * void makeSound() {
+         * System.out.println("Dog barks");
+         * }
+         * }
+         * 
+         * | Abstract Class | Child Classes |
+         * | -------------- | ----------------- |
+         * | Animal | Dog, Cat |
+         * | Vehicle | Car, Bike |
+         * | Shape | Circle, Rectangle |
+         * 
+         */
+
+        CreditCard card = new CreditCard();
+        UPI upi = new UPI();
+
+        card.showMessage();
+        card.makePayment();
+        System.out.println("--------------------------------------");
+        upi.showMessage();
+        upi.makePayment();
+    }
+}
+
+abstract class Payment {
+    abstract void makePayment();
+
+    void showMessage() {
+        System.out.println("Processing Payment");
+    }
+}
+
+class CreditCard extends Payment {
+    @Override
+    void makePayment() {
+        System.out.println("Payment done using Credit Card");
+    }
+}
+
+class UPI extends Payment {
+    @Override
+    void makePayment() {
+        System.out.println("Payment done using UPI");
     }
 }
